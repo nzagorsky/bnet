c011d81d8c096c23f80af655584d0364d35d1819
Nikita <toltenos@gmail.com>
2017-12-30 04:18:35 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='bnet',
+    packages=['bnet'],
+    version='0.1',
+    description='Battle.net API Wrapper',
+    author='Nikita Zagorskiy',
+    author_email='toltenos@gmail.com',
+    url='https://github.com/nzagorsky/bnet',
+    download_url='https://github.com/nzagorsky/bnet/archive/0.1.tar.gz',
+    keywords=['battle.net', 'warcraft', 'diablo', 'sc2', 'wow'],
+    classifiers=[],
+)
